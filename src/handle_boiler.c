f7b89556e226dde991ba82a1b55cadf5263acdab
joseph skokan <joeskokan20@gmail.com>
2025-05-06 00:54:13 -0400

    boilers

--- src/handle_boiler.c
+++ src/handle_boiler.c
@@ -1,2 +1,14 @@
 #include "../headers/handle_boilers.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void handle_boilers(char *cmd)
+{
+    char filename[100];
+    if (sscanf(cmd, "boiler %s", filename) != 1)
+    {
+        printf("Usage: boilers <filename.ext>\n");
+        return;
+    }
+}

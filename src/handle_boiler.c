ee78b46669c5b37403de3deb3be1569ab4a93a1d
joseph skokan <joeskokan20@gmail.com>
2025-05-06 01:03:29 -0400

    changes to boiler

--- src/handle_boiler.c
+++ src/handle_boiler.c
@@ -25,4 +25,28 @@ void handle_boilers(char *cmd)
         perror("Error creating file");
         return;
     }
+    // extension based boilerplate
+    if (strcmp(ext, ".c") == 0)
+    {
+        fprintf(file, "#include <stdio.h>\n\nint main() {\n    printf(\"Hello, world!\\n\");\n    return 0;\n}\n");
+    }
+    else if (strcmp(ext, ".py") == 0)
+    {
+        fprintf(file, "#!/usr/bin/env python3\n\nprint(\"Hello, world!\")\n");
+    }
+    else if (strcmp(ext, ".js") == 0)
+    {
+        fprintf(file, "console.log(\"Hello, world!\");\n");
+    }
+    else if (strcmp(ext, ".html") == 0)
+    {
+        fprintf(file, "<!DOCTYPE html>\n<html>\n<head><title>Boilerplate</title></head>\n<body>\n<h1>Hello, world!</h1>\n</body>\n</html>\n");
+    }
+    else
+    {
+        fprintf(file, "// Boilerplate for %s not available\n", ext);
+    }
+
+    fclose(file);
+    printf("Boilerplate created in '%s'.\n", filename);
 }
